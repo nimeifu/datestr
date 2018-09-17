adc58d389b01c796361b31ad85e19269f3a59176
BT_1N3_16@BKTECH.LOCAL <BT_1N3_16@BKTECH.LOCAL>
2018-09-17 10:19:36 -0400

    Initial commit

---
+++ src/datestr.java
@@ -0,0 +1,11 @@
+public class datestr {
+    public static String datestr()
+    {
+        String dateformat="mm/dd/yyyy";
+        String slash="/";
+        int index=dateformat.indexof(slash);
+        String slash1=dateformat.
+
+
+    }
+}
